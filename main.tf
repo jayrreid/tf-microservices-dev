ac73b07dbf2a65a2472fc4b9c74c343206472c69
jay.r.reid@gmail.com <jay.r.reid@gmail.com>
2020-06-27 08:48:45 -0400

    fixed variables

--- main.tf
+++ main.tf
@@ -1,78 +1,23 @@
-terraform {
-  required_version = ">= 0.12.0"
-}
 
 provider "aws" {
   version = ">= 2.28.1"
   region  = var.region
 }
 
-provider "random" {
-  version = "~> 2.1"
-}
-
-provider "local" {
-  version = "~> 1.2"
-}
-
-provider "null" {
-  version = "~> 2.1"
-}
-
-provider "template" {
-  version = "~> 2.1"
-}
-
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-  version                = "~> 1.11"
-}
-
-data "aws_availability_zones" "available" {
-}
-
-locals {
-  cluster_name = "test-eks-${random_string.suffix.result}"
-}
-
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
+//--------------------------------------------------------------------
+// Modules
 
 module "vpc1" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "2.6.0"
+  source  = "app.terraform.io/koinonia/vpc1/aws"
+  version = "1.0.0"
 
-  name                 = "test-vpc"
-  cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = "1"
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = "1"
-  }
+  cidr = "${var.vpc1_cidr}"
+  cluster_name = "${var.eks_cluster_name}"
+  master_subnet_cidr = "${var.vpc1_master_subnet_cidr}"
+  private_subnet_cidr = "${var.vpc1_private_subnet_cidr}"
+  public_subnet_cidr = "${var.vpc1_public_subnet_cidr}"
+  vpc_name = "${var.vpc1_vpc_name}"
+  worker_subnet_cidr = "${var.vpc1_worker_subnet_cidr}"
 }
 
 
@@ -81,6 +26,6 @@ module "eks" {
   version = "1.0.0"
 
   cluster_name = "${var.eks_cluster_name}"
-  subnets      = module.vpc1.private_subnets
+  subnets = flatten([module.vpc1.master_subnet, module.vpc1.worker_node_subnet])
   vpc_id = module.vpc1.vpc_id
 }
