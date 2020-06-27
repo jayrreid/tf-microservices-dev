1c05c6f01f02b40b2e7a90bef5a1c4838f7b0ee2
jay.r.reid@gmail.com <jay.r.reid@gmail.com>
2020-06-26 22:00:43 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+
+//--------------------------------------------------------------------
+// Modules
+
+module "vpc1" {
+  source  = "app.terraform.io/koinonia/vpc1/aws"
+  version = "1.0.0"
+
+  cidr = "${var.vpc1_cidr}"
+  cluster_name = "${var.eks_cluster_name}"
+  master_subnet_cidr = "${var.vpc1_master_subnet_cidr}"
+  private_subnet_cidr = "${var.vpc1_private_subnet_cidr}"
+  public_subnet_cidr = "${var.vpc1_public_subnet_cidr}"
+  vpc_name = "${var.vpc1_vpc_name}"
+  worker_subnet_cidr = "${var.vpc1_worker_subnet_cidr}"
+}
+
+module "eks" {
+  source  = "app.terraform.io/koinonia/eks/aws"
+  version = "1.0.0"
+
+  cluster_name = "${var.eks_cluster_name}"
+  permissions_boundary = "${var.eks_permissions_boundary}"
+  subnets = ["${module.vpc1.master_subnet}", "${module.vpc1.worker_node_subnet}"]
+  vpc_id = "${var.eks_vpc_id}"
+}
---
+++ variables.tf
@@ -0,0 +1,48 @@
+//--------------------------------------------------------------------
+// Variables
+variable "eks_permissions_boundary" {
+  description ="permission boundary attached"
+}
+
+variable "eks_vpc_id" {
+  description = "Description: VPC where the cluster and workers will be deployed."
+}
+
+variable "eks_cluster_name" {
+  description = "cluster name"
+}
+
+variable "eks_subnets" {
+  description ="Description: A list of subnets to place the EKS cluster and workers within."
+  type = list(string)
+}
+
+variable "vpc1_vpc_name" {}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC."
+}
+
+#subnet variable
+variable "vpc1_master_subnet_cidr" {
+  type        = list(string)
+  description = "CIDR for master subnet"
+  default     = []
+}
+
+variable "vpc1_worker_subnet_cidr" {
+  type        = list(string)
+  description = "CIDR for worker subnet"
+  default     = []
+}
+
+variable "vpc1_public_subnet_cidr" {
+  description = "Kubernetes Public CIDR"
+  type        = list(string)
+}
+
+variable "vpc1_private_subnet_cidr" {
+  type        = list(string)
+  description = "Kubernetes Private CIDR"
+  default     = []
+}
