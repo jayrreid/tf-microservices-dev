9187ecf947d4eadfcc9b935a2a25f94327cda65a
jay.r.reid@gmail.com <jay.r.reid@gmail.com>
2020-07-03 15:41:03 -0400

    added istio configuration to main.tf

--- istio.tf
+++ istio.tf
@@ -2,7 +2,6 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
   version                = "~> 1.11"
 }
 
@@ -15,12 +14,12 @@ resource "kubernetes_namespace" "istio_system" {
 provider "helm" {
 
   kubernetes {
-    load_config_file = false
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
     token                  = data.aws_eks_cluster_auth.cluster.token
     load_config_file       = false
     version                = "~> 1.11"
+    load_config_file = false
   }
 }
 
