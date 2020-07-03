e181ce6d80dc28c390fd270a9c3ba747f4f01a68
jay.r.reid@gmail.com <jay.r.reid@gmail.com>
2020-07-03 15:41:49 -0400

    added istio configuration to main.tf

--- istio.tf
+++ istio.tf
@@ -1,10 +1,3 @@
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  version                = "~> 1.11"
-}
-
 resource "kubernetes_namespace" "istio_system" {
   metadata {
     name = "istio-system"
