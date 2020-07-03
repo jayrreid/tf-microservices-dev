3d6f7cb3e7499fd0cc476fc5d6f10655ce7e5c73
jay.r.reid@gmail.com <jay.r.reid@gmail.com>
2020-07-03 15:44:13 -0400

    added istio configuration to main.tf

--- istio.tf
+++ istio.tf
@@ -11,7 +11,6 @@ provider "helm" {
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
     token                  = data.aws_eks_cluster_auth.cluster.token
     load_config_file       = false
-    version                = "~> 1.11"
   }
 }
 
