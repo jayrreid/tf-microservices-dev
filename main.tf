8dac206cfb94cf029e2157566f1070c5d0dab606
jay.r.reid@gmail.com <jay.r.reid@gmail.com>
2020-07-03 13:04:22 -0400

    added istio configuration to main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "helm_release" "istio" {
   repository = "https://storage.googleapis.com/istio-release/releases/1.5.4/charts/"
   version    = "1.5.4"
   chart      = "istio"
-  namespace  = "${kubernetes_namespace.istio.metadata.0.name}"
+  namespace  = "kubernetes_namespace.istio.metadata.0.name"
 }
 
 
