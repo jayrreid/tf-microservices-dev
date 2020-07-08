e65506f053fdd6519aa28b1d16185fee05aeaa4e
jay.r.reid@gmail.com <jay.r.reid@gmail.com>
2020-07-08 09:48:42 -0400

    added gremlin

--- gremlin.tf
+++ gremlin.tf
@@ -10,11 +10,31 @@ resource "helm_release" "gremlin" {
   name = "gremlin"
   repository = "https://helm.gremlin.com/"
   chart = "gremlin"
-  gremlin.secret.managed="true"
-  gremlin.secret.type = "secret"
-  gremlin.secret.teamID = "e61b3523-2466-5dcf-b601-b4a3f1eb8780"
-  gremlin.secret.clusterID = module.eks.cluster_id
-  gremlin.secret.teamSecret = "21fa870a-5472-4aae-ba87-0a5472daae17"
+
+  set {
+    name  = "gremlin.secret.managed"
+    value = "true"
+  }
+
+  set {
+    name  = "gremlin.secret.type"
+    value = "secret"
+  }
+
+  set_string {
+    name  = "gremlin.secret.teamID"
+    value = "e61b3523-2466-5dcf-b601-b4a3f1eb8780"
+  }
+
+  set_string {
+    name  = "gremlin.secret.teamSecret"
+    value = "21fa870a-5472-4aae-ba87-0a5472daae17
+  }
+
+  set_string {
+    name  = "gremlin.secret.clusterID"
+    value = module.eks.cluster_id
+  }
 
   namespace = "${kubernetes_namespace.gremlin.metadata.0.name}"
 
